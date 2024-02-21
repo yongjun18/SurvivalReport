6298d33e6cba45a3460b558476fb79ee1372ee58
yongjun18 <yongjun18976@gmail.com>
2024-02-21 13:06:15 +0900

    git 초기 세팅

---
+++ SurvivalReport/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SurvivalReport
+//
+//  Created by yongjun18 on 2/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SurvivalReport/SurvivalReportApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurvivalReportApp.swift
+//  SurvivalReport
+//
+//  Created by yongjun18 on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SurvivalReportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
