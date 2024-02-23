9a8f71f9fec230f073a4d304df92b3e364def11c
yongjun18 <yongjun18976@gmail.com>
2024-02-23 16:55:36 +0900

    설정 화면 초안

--- SurvivalReport/View/SettingView.swift
+++ SurvivalReport/View/SettingView.swift
@@ -12,9 +12,54 @@ struct SettingView: View {
         VStack(spacing: 0) {
             TitleBar(title: "설정")
             ScrollView {
-                Text("Setting View")
+                VStack(spacing: 0) {
+                    
+                    Spacer().frame(height: 5)
+                    
+                    SettingItemView(name: "다크모드")
+                    Spacer().frame(height: 1)
+                    SettingItemView(name: "화면잠금설정")
+                    Spacer().frame(height: 1)
+                    SettingItemView(name: "알림설정")
+                    Spacer().frame(height: 1)
+                    SettingItemView(name: "혜택 알림설정")
+                    Spacer().frame(height: 1)
+                    SettingItemView(name: "비밀번호 변경")
+                    
+                    Spacer().frame(height: 5)
+                    
+                    SettingItemView(name: "도움말")
+                    Spacer().frame(height: 1)
+                    SettingItemView(name: "이용약관")
+                    Spacer().frame(height: 1)
+                    SettingItemView(name: "개인정보 처리방침")
+                    Spacer().frame(height: 1)
+                    SettingItemView(name: "버전")
+                    
+                    Spacer().frame(height: 5)
+                    
+                    SettingItemView(name: "인증 초기화")
+                }
+                .frame(maxWidth: .infinity)
             }
+            .background(Color(hex: "#cccccc"))
         }
+    }
+}
+
+struct SettingItemView: View {
+    let name: String
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            Text(name)
+            Spacer()
+            Image(systemName: "chevron.right")
+                .foregroundStyle(Color(hex:"#555555"))
+        }
+        .font(.system(size: 16))
+        .padding(15)
+        .background(Color.white)
     }
 }
 
